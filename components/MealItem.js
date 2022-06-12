47fb0a77b713de1c145bbc54499b893fe85490d3
peter_alfie <alfie1104@gmail.com>
2022-06-13 08:47:26 +0900

    feat(mealitem, mealsoverviewscreen): displaying Meals

---
+++ components/MealItem.js
@@ -0,0 +1,10 @@
+import { View, Text, StyleSheet } from "react-native";
+function MealItem({ title }) {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  );
+}
+
+export default MealItem;
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
 
 function MealsOverViewScreen({ route }) {
@@ -8,9 +9,21 @@ function MealsOverViewScreen({ route }) {
   */
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
