967eec9b368b0c81310d98c854ccbb4f196a37e3
peter_alfie <alfie1104@gmail.com>
2022-06-13 08:32:42 +0900

    feat(mealsoverviewscreen, categoriesscreen): working with Route Parameters to pass data

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,9 +1,16 @@
 import { View, Text, StyleSheet } from "react-native";
 import { MEALS } from "../data/dummy-data";
-function MealsOverViewScreen() {
+
+function MealsOverViewScreen({ route }) {
+  /*
+    If the component is not the direct children of Stack.Screen and the component is nested component,
+    then we can use useRoute as an alternative way of using route parameter
+  */
+  const catId = route.params.categoryId;
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <Text>Meals Overview Screen - {catId}</Text>
     </View>
   );
 }
